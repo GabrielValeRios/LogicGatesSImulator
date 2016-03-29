282aa72bde157bae48192b1c89f205d892eb8cf2
engcarlosrosa <kdu.carloseduardo@gmail.com>
2016-03-29 19:23:20 -0300

    classe Switch adicionada

---
+++ SimuladorPortasLógicas/src/Switch.java
@@ -0,0 +1,14 @@
+package projeto2Versao2Carlos;
+
+public class Switch {
+	 boolean on;
+	
+	public void setOn(boolean a){
+		this.on = a;
+	}
+	
+	public boolean getOn(){
+		return this.on;
+	}
+
+}
